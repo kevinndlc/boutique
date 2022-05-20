dfda3815e8275ff8f3489e4be8c3e42d350d73b6
Kevin <kevin@nedellec.eu>
2022-05-20 10:42:52 +0200

    Feat: Cart Store

--- src/features/boutique/stores/cartStore.ts
+++ src/features/boutique/stores/cartStore.ts
@@ -1,12 +1,48 @@
-import type { ProductIntf } from '@/shared/interfaces';
+import type { ProductCartIntf } from '@/shared/interfaces';
 import { defineStore } from 'pinia';
+import { useProducts } from './productStore';
 
 interface CartState {
-  cart: ProductIntf[];
+  cart: ProductCartIntf[];
 }
 
 export const useCart = defineStore('cart', {
   state: (): CartState => ({
     cart: [],
   }),
+  actions: {
+    addProductToCart(productId: string): void {
+      const productStore = useProducts();
+      const product = productStore.products.find(
+        (product) => product._id === productId
+      );
+      if (product) {
+        const productInCart = this.cart.find(
+          (product) => product._id === productId
+        );
+        if (productInCart) {
+          productInCart.quantity++;
+        } else {
+          this.cart.push({ ...product, quantity: 1 });
+        }
+      }
+    },
+    removeProductFromCart(productId: string): void {
+      const productFromCartIndex = this.cart.findIndex(
+        (product) => product._id === productId
+      );
+      if (productFromCartIndex !== -1) {
+        if (this.cart[productFromCartIndex].quantity === 1) {
+          this.cart.splice(productFromCartIndex, 1);
+        } else {
+          this.cart[productFromCartIndex].quantity--;
+        }
+      }
+    }
+  },
+  getters: {
+    cartEmpty(state): boolean {
+      return state.cart.length === 0;
+    },
+  },
 });
--- src/features/boutique/stores/productStore.ts
+++ src/features/boutique/stores/productStore.ts
@@ -24,3 +24,43 @@ export const useProducts = defineStore('products', {
     moreResults: true
   })
 })
+
+// watchEffect(async () => {
+//   state.isLoading = true;
+//   const products = await fetchProducts(state.filters, state.page);
+//   if (Array.isArray(products)) {
+//     state.products = [...state.products, ...products];
+//     if (products.length < 20) {
+//       state.moreResults = false;
+//     } else {
+//       state.moreResults = true;
+//     }
+//   } else {
+//     state.products = [...state.products, products];
+//   }
+//   state.isLoading = false;
+// });
+
+// const updateFilter = (filterUpdate: FilterUpdateIntf): void => {
+//   if (filterUpdate.search !== undefined) {
+//     state.filters.search = filterUpdate.search;
+//   } else if (filterUpdate.priceRange) {
+//     state.filters.priceRange = filterUpdate.priceRange;
+//   } else if (filterUpdate.category) {
+//     state.filters.category = filterUpdate.category;
+//   } else {
+//     state.filters = { ...DEFAULT_FILTERS };
+//   }
+// };
+
+// const filteredProducts = computed(() => {
+//   return state.products.filter((product) => {
+//     if (
+//       product.title.toLowerCase().startsWith(state.filters.search.toLowerCase())
+//     ) {
+//       return true;
+//     } else {
+//       return false;
+//     }
+//   });
+// });
