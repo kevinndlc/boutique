37dadf30f1e5176f3383adaea337dc126edd1220
Kevin <kevin@nedellec.eu>
2022-05-25 00:06:18 +0200

    Feat: Add MongoDB

--- server/index.js
+++ server/index.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const path = require('path');
 const port = process.env.PORT ?? '3000';
-const app = express();
+const { MongoClient } = require('mongodb');
 
+const client = new MongoClient(
+  'mongodb+srv://kevinndlc:123@cluster0.o1kje.mongodb.net/?retryWrites=true&w=majority'
+);
+client.connect()
+  .then(() => {
+    console.log('Connected!');
+  })
+  .catch(err => {
+    console.error(err);
+  })
+
+const app = express();
 app.use(express.static('../client/dist'));
 
 app.get('*', (req, res) => {
